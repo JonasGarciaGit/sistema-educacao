a08ebec0ea5978e136468a48b4182e1390e97b59
IsaqueGarcia <isaqueemprego1@gmail.com>
2019-08-20 18:30:02 -0300

    Modificações classes de dominio

--- src/main/java/com/apiedu/apiedu/domain/Professor.java
+++ src/main/java/com/apiedu/apiedu/domain/Professor.java
@@ -17,12 +17,13 @@ public class Professor {
 	public Professor() {
 	}
 
-	public Professor(Integer id, String nome, String email, String telefone, String endereco) {
+	public Professor(Integer id, String nome, String email, String telefone, String endereco, Curso curso) {
 		this.id = id;
 		this.nome = nome;
 		this.email = email;
 		this.telefone = telefone;
 		this.endereco = endereco;
+		this.curso = curso;
 	}
 
 	public Integer getId() {
@@ -65,10 +66,46 @@ public class Professor {
 		this.endereco = endereco;
 	}
 
+
+	public Curso getCurso() {
+		return curso;
+	}
+
+	public void setCurso(Curso curso) {
+		this.curso = curso;
+	}
+	
 	@Override
 	public String toString() {
 		return "Professor [id=" + id + ", nome=" + nome + ", email=" + email + ", telefone=" + telefone + ", endereco="
 				+ endereco + ", at=" + atv + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Professor other = (Professor) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 	
+	
+
 }
