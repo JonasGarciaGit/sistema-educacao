439791d75fa56688967e5efb2d996085d27e81e7
kloppos120 <jonasemprego1@gmail.com>
2019-08-20 18:24:31 -0300

    Realizando composicao Aluno/Curso

--- src/main/java/com/apiedu/apiedu/domain/Aluno.java
+++ src/main/java/com/apiedu/apiedu/domain/Aluno.java
@@ -1,5 +1,8 @@
 package com.apiedu.apiedu.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aluno {
 	
 	private String nome;
@@ -8,5 +11,136 @@ public class Aluno {
 	private Integer id;
 	private String telefone;
 	private String endereco;
+	
+	
+	private Curso curso;
+	
+	private List<Atividade> atividades = new ArrayList<>();
+	
+	
+	public Aluno() {
+		
+	}
+
+
+	public Aluno(String nome, String email, Integer idade, Integer id, String telefone, String endereco, Curso curso) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.idade = idade;
+		this.id = id;
+		this.telefone = telefone;
+		this.endereco = endereco;
+		this.curso = curso;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+
+	public Curso getCurso() {
+		return curso;
+	}
+
+
+	public void setCurso(Curso curso) {
+		this.curso = curso;
+	}
+
+
+	public List<Atividade> getAtividades() {
+		return atividades;
+	}
+
+
+	public void setAtividades(List<Atividade> atividades) {
+		this.atividades = atividades;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
 	
 }
--- src/main/java/com/apiedu/apiedu/domain/Curso.java
+++ src/main/java/com/apiedu/apiedu/domain/Curso.java
@@ -1,10 +1,105 @@
 package com.apiedu.apiedu.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Curso {
 
 	private Integer id;
 	private String nome;
 	private String descricao;
 	private Float duracao;
+	
+	private List<Aluno> alunos = new ArrayList<>();
+	
+	private Professor professor;
+	
+	public Curso() {
+		
+	}
+
+	public Curso(Integer id, String nome, String descricao, Float duracao, Professor professor) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.duracao = duracao;
+		this.professor = professor;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Float getDuracao() {
+		return duracao;
+	}
+
+	public void setDuracao(Float duracao) {
+		this.duracao = duracao;
+	}
+
+	public List<Aluno> getAlunos() {
+		return alunos;
+	}
+
+	public void setAlunos(List<Aluno> alunos) {
+		this.alunos = alunos;
+	}
+
+	public Professor getProfessor() {
+		return professor;
+	}
+
+	public void setProfessor(Professor professor) {
+		this.professor = professor;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Curso other = (Curso) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
 	
 }
