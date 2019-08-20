3bd9acc67728fb49cee40b6966f875462fce38b3
IsaqueGarcia <isaqueemprego1@gmail.com>
2019-08-20 18:21:55 -0300

    Entidade Atividade e Professor composta

--- src/main/java/com/apiedu/apiedu/domain/Aluno.java
+++ src/main/java/com/apiedu/apiedu/domain/Aluno.java
@@ -2,10 +2,10 @@ package com.apiedu.apiedu.domain;
 
 public class Aluno {
 	
-	private String nome;
-	private String email;
-	private Integer idade;
 	private Integer id;
+	private String nome;
+	private Integer idade;
+	private String email;
 	private String telefone;
 	private String endereco;
 	
--- src/main/java/com/apiedu/apiedu/domain/Professor.java
+++ src/main/java/com/apiedu/apiedu/domain/Professor.java
@@ -1,5 +1,8 @@
 package com.apiedu.apiedu.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Professor {
 
 	private Integer id;
@@ -7,5 +10,65 @@ public class Professor {
 	private String email;
 	private String telefone;
 	private String endereco;
+	
+	List<Atividade> atv = new ArrayList<Atividade>();
+	private Curso curso;
+	
+	public Professor() {
+	}
+
+	public Professor(Integer id, String nome, String email, String telefone, String endereco) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+		this.endereco = endereco;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	@Override
+	public String toString() {
+		return "Professor [id=" + id + ", nome=" + nome + ", email=" + email + ", telefone=" + telefone + ", endereco="
+				+ endereco + ", at=" + atv + "]";
+	}
 	
 }
