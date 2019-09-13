0b0496a49c8db0a71330e21a380a6c03ef706ffb
IsaqueGarcia <isaqueemprego1@gmail.com>
2019-09-13 16:00:50 -0300

    Delete atividade atualizado para DeleteById

--- src/main/java/com/apiedu/apiedu/resources/AtividadeController.java
+++ src/main/java/com/apiedu/apiedu/resources/AtividadeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -47,8 +48,8 @@ public class AtividadeController {
 	}
 
 	@ApiOperation(value = "Deleta uma atividade.")
-	@DeleteMapping(value = "/atividade")
-	public void deletar(@RequestBody Atividade atividade) {
-		service.deletarAtividade(atividade);
+	@DeleteMapping(value = "/atividade/{id}")
+	public void deletar(@PathVariable Integer id) {
+		service.deletarAtividade(id);
 	}
 }
